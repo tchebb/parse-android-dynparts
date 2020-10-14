e24ade28db78fa16f1453653142987af0640cbde
Thomas Hebb <tommyhebb@gmail.com>
2020-10-14 04:35:28 -0700

    Initial implementation of executable

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include <liblp/liblp.h>
 
 #include <iostream>
+#include <sstream>
+#include <string_view>
 
 using namespace android::fs_mgr;
 
@@ -8,9 +10,12 @@ using std::cerr;
 using std::cout;
 using std::endl;
 
-int main(int argc, char *argv[]) {
+constexpr std::string_view kDmPrefix = "dynpart-";
+
+int main(int argc, char* argv[]) {
   if (argc != 2) {
-    cerr << "Usage: " << argv[0] << " device" << endl;
+    cerr << "Usage: dmsetup create --concise \"$(" << argv[0] << " device)\""
+         << endl;
     return 1;
   }
 
@@ -20,9 +25,57 @@ int main(int argc, char *argv[]) {
     return 1;
   }
 
-  for (auto p : metadata->partitions) {
-    cout << p.name << endl;
+  std::string table;
+
+  // Code structure taken from Android's system/core/fs_mgr/fs_mgr_dm_linear.cpp
+  for (auto partition : metadata->partitions) {
+    if (!partition.num_extents) {
+      cerr << "Skipping zero-length logical partition: "
+           << GetPartitionName(partition) << endl;
+      continue;
+    }
+    if (partition.attributes & LP_PARTITION_ATTR_DISABLED) {
+      cerr << "Skipping disabled partition: " << GetPartitionName(partition)
+           << endl;
+      continue;
+    }
+
+    std::ostringstream line;
+    bool read_only = partition.attributes & LP_PARTITION_ATTR_READONLY;
+    line << kDmPrefix << GetPartitionName(partition) << ",,,"
+         << (read_only ? "ro" : "rw");
+
+    uint64_t sector = 0;
+    for (size_t i = 0; i < partition.num_extents; i++) {
+      const auto& extent = metadata->extents[partition.first_extent_index + i];
+      switch (extent.target_type) {
+        case LP_TARGET_TYPE_ZERO:
+          line << "," << sector << " " << extent.num_sectors << " zero";
+          break;
+        case LP_TARGET_TYPE_LINEAR: {
+          if (extent.target_source != 0) {
+            cerr << "This utility does not yet support multiple block devices"
+                 << endl;
+            return 1;
+          }
+
+          line << "," << sector << " " << extent.num_sectors << " linear "
+               << argv[1] << " " << extent.target_data;
+          break;
+        }
+        default:
+          cerr << "Unknown target type in metadata: " << extent.target_type
+               << endl;
+          return false;
+      }
+      sector += extent.num_sectors;
+    }
+
+    if (!table.empty()) table += ";";
+    table += line.str();
   }
+
+  cout << table << endl;
 
   return 0;
 }
